70f6b63e1c314f283cdc422a9f7d66f32645c673
rayzaranza <rayzaranza@gmail.com>
2026-04-09 14:39:18 -0300

    build(infra): setup cloudflare deployment

--- astro.config.ts
+++ astro.config.ts
@@ -1,3 +1,11 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+import cloudflare from "@astrojs/cloudflare";
+
+export default defineConfig({
+  trailingSlash: "never",
+  build: {
+    format: "file",
+  },
+  adapter: cloudflare({ imageService: "cloudflare" }),
+});
