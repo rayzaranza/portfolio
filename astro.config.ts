628b33402b700db9ba8ee209c30036c14efe2609
Ray Zaranza <rayzaranza@gmail.com>
2026-04-10 03:38:14 -0300

    feat: setup main layout and design tokens

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,36 @@
-import { defineConfig } from "astro/config";
-
+import { defineConfig, fontProviders } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Geologica",
+      cssVariable: "--font-family",
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/Geologica-Bold.woff2"],
+            weight: 700,
+            style: "normal",
+            display: "swap",
+          },
+          {
+            src: ["./src/assets/fonts/Geologica-Medium.woff2"],
+            weight: 500,
+            style: "normal",
+            display: "swap",
+          },
+          {
+            src: ["./src/assets/fonts/Geologica-Regular.woff2"],
+            weight: 400,
+            style: "normal",
+            display: "swap",
+          },
+        ],
+      },
+    },
+  ],
   trailingSlash: "never",
   build: {
     format: "file",
---
+++ src/projects/site-gadioli.md
@@ -0,0 +1,15 @@
+---
+title: "Site da Gadioli"
+slug: "site-gadioli"
+image:
+  url: "https://docs.astro.build/assets/rose.webp"
+  alt: "Captura de tela do site da Gadioli"
+tags: ["ux/ui", "front-end"]
+---
+
+SAdhaslk çhjaskluh ahd alkjsyd akdusaghd kasudghas sdaSD df dasd sadals **gdsbkl** agvkjadga lsfvdajks ad jasgdk gasgd akdgakd gasgd kaysd as. ashdalG KALUgd kalsgudA Dhasd aljfda das.
+
+## askdagyasgd
+
+1. **asdlahusahda** alçidha sça açjd
+2. ahgldhs&&
